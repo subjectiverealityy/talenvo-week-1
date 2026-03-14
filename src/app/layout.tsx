d21f560a46de60e0b444bd6b8fa9fb9d72963a91
Oge <subjectiverealityy@gmail.com>
2026-03-14 13:25:39 +0100

    remove StoreProvider wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,16 @@
 import type { Metadata } from "next";
 import "@/app/styles/globals.css";
-import { StoreProvider } from "@/store/store";
 
 export const metadata: Metadata = {
-  title: "BoardList - Talenvo Project (Week 1)",
-  description: "Ogechukwu Onuora's submission to the Talenvo Global Residency Program's week 1 task",
+  title: "BoardList - Talenvo Project",
+  description: "Ogechukwu Onuora's submission to the Talenvo Global Residency Program's week 2 task",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <StoreProvider>
           {children}
-        </StoreProvider>
       </body>
     </html>
   );
